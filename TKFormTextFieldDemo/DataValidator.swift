c71fdae79aff818a806ab7849f718e6264517060
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2017-01-01 19:14:59 +0800

    Add login demo page

---
+++ TKFormTextFieldDemo/DataValidator.swift
@@ -0,0 +1,39 @@
+//
+//  DataValidator.swift
+//  TKFormTextFieldDemo
+//
+//  Created by Thongchai Kolyutsakul on 1/1/17.
+//  Copyright © 2017 Viki. All rights reserved.
+//
+
+import Foundation
+
+// Stores functions that validates input text.
+// Each function takes a text.
+// Returns an error string if that text is invalid, or nil if valid.
+class DataValidator {
+  class func email(text: String?) -> String? {
+    guard let text = text, !text.isEmpty else {
+      return "Email is missing."
+    }
+    guard text.tk_isValidEmail() else {
+      return "Email is invalid."
+    }
+    return nil
+  }
+  
+  class func password(text: String?) -> String? {
+    guard let text = text, text.characters.count >= 6 else {
+      return "Password is invalid."
+    }
+    return nil
+  }
+}
+
+private extension String {
+  func tk_isValidEmail() -> Bool {
+    let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+    let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+    return emailTest.evaluate(with: self)
+  }
+}
--- TKFormTextFieldDemo/ViewController.swift
+++ TKFormTextFieldDemo/ViewController.swift
@@ -10,16 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var emailTextField: TKFormTextField!
+  @IBOutlet weak var passwordTextField: TKFormTextField!
+  @IBOutlet weak var submitButton: UIButton!
+  
+  let submitSegueId = "submit"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.emailTextField.placeholder = "Email"
+    self.emailTextField.addTarget(self, action: #selector(clearErrorMessageIfNeeded), for: .editingChanged)
+    self.emailTextField.addTarget(self, action: #selector(updateErrorMessage), for: .editingDidEnd)
+    self.emailTextField.enablesReturnKeyAutomatically = true
+    self.emailTextField.returnKeyType = .next
+    self.emailTextField.delegate = self
+    
+    self.passwordTextField.placeholder = "Password"
+    self.passwordTextField.addTarget(self, action: #selector(clearErrorMessageIfNeeded), for: .editingChanged)
+    self.passwordTextField.addTarget(self, action: #selector(updateErrorMessage), for: .editingDidEnd)
+    self.passwordTextField.enablesReturnKeyAutomatically = true
+    self.passwordTextField.returnKeyType = .done
+    self.passwordTextField.delegate = self
+    
+    self.submitButton.isEnabled = isAllTextFieldsValid()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func updateErrorMessage(textField: TKFormTextField) {
+    if textField == emailTextField {
+      textField.errorMessage = DataValidator.email(text: textField.text)
+    } else if textField == passwordTextField {
+      textField.errorMessage = DataValidator.password(text: textField.text)
+    }
+    self.submitButton.isEnabled = isAllTextFieldsValid()
   }
+  
+  func clearErrorMessageIfNeeded(textField: TKFormTextField) {
+    if textField == emailTextField {
+      if DataValidator.email(text: textField.text) == nil {
+        textField.errorMessage = nil
+      }
+    } else if textField == passwordTextField {
+      if DataValidator.password(text: textField.text) == nil {
+        textField.errorMessage = nil
+      }
+    }
+    self.submitButton.isEnabled = isAllTextFieldsValid()
+  }
+  
+  private func isAllTextFieldsValid() -> Bool {
+    guard DataValidator.email(text: emailTextField.text) == nil else { return false }
+    guard DataValidator.password(text: passwordTextField.text) == nil else { return false }
+    return true
+  }
+  
+  @IBAction func submit(_ sender: Any) {
+    self.performSegue(withIdentifier: submitSegueId, sender: nil)
+  }
+  
+}
 
-
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if (textField == emailTextField) {
+      _ = passwordTextField.becomeFirstResponder()
+    }
+    else if (textField == passwordTextField) {
+      _ = passwordTextField.resignFirstResponder()
+      if self.submitButton.isEnabled {
+        submit(submitButton)
+      }
+    }
+    return false
+  }
 }
 
