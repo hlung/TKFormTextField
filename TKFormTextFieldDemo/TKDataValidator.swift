df10f51d07327b0c08760cb45452fefd2d46a10c
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2017-01-02 16:56:59 +0800

    Create TKDataValidator

--- TKFormTextFieldDemo/TKDataValidator.swift
+++ TKFormTextFieldDemo/TKDataValidator.swift
@@ -8,10 +8,69 @@
 
 import Foundation
 
+protocol TKDataValidatorDelegate: class {
+  func dataValidator(validator: TKDataValidator, isAllValid: Bool)
+}
+
 // Stores functions that validates input text.
 // Each function takes a text.
 // Returns an error string if that text is invalid, or nil if valid.
 class TKDataValidator {
+  
+  let textFields: [TKFormTextField]
+  let validators: [((String?) -> String?)]
+  weak var delegate: TKDataValidatorDelegate?
+  
+  var isAllValid: Bool = false {
+    didSet {
+      self.delegate?.dataValidator(validator: self, isAllValid: self.isAllValid)
+    }
+  }
+  
+  init(textFields: [TKFormTextField], validators: [((String?) -> String?)]) {
+    self.textFields = textFields
+    self.validators = validators
+    for tf in textFields {
+      addTargetForErrorUpdating(tf)
+    }
+  }
+  
+  fileprivate func addTargetForErrorUpdating(_ textField: TKFormTextField) {
+    textField.addTarget(self, action: #selector(clearErrorIfNeeded), for: .editingChanged)
+    textField.addTarget(self, action: #selector(updateError), for: .editingDidEnd)
+  }
+  
+  @objc fileprivate func updateError(textField: TKFormTextField) {
+    textField.error = validationError(textField)
+    self.isAllValid = isAllTextFieldsValid()
+  }
+  
+  @objc fileprivate func clearErrorIfNeeded(textField: TKFormTextField) {
+    if validationError(textField) == nil {
+      textField.error = nil
+    }
+    self.isAllValid = isAllTextFieldsValid()
+  }
+  
+  fileprivate func isAllTextFieldsValid() -> Bool {
+    for tf in textFields {
+      if validationError(tf) != nil {
+        return false
+      }
+    }
+    return true
+  }
+  
+  private func validationError(_ textField: TKFormTextField) -> String? {
+    guard let index = self.textFields.index(of: textField) else { return "Error. Cannot validate." }
+    let validator = self.validators[index]
+    return validator(textField.text)
+  }
+}
+
+// MARK: Validators
+extension TKDataValidator {
+  
   class func email(text: String?) -> String? {
     guard let text = text, !text.isEmpty else {
       return "Email is missing."
@@ -28,6 +87,7 @@ class TKDataValidator {
     }
     return nil
   }
+  
 }
 
 private extension String {
--- TKFormTextFieldDemo/ViewController.swift
+++ TKFormTextFieldDemo/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   @IBOutlet weak var emailTextField: TKFormTextField!
   @IBOutlet weak var passwordTextField: TKFormTextField!
   @IBOutlet weak var submitButton: UIButton!
+  
+  var dataValidator: TKDataValidator!
   
   let submitSegueId = "submit"
   
@@ -32,10 +34,6 @@ class ViewController: UIViewController {
     self.passwordTextField.delegate = self
     self.passwordTextField.isSecureTextEntry = true
     
-    // Validation logic
-    self.addTargetForErrorUpdating(self.emailTextField)
-    self.addTargetForErrorUpdating(self.passwordTextField)
-    
     // Customize labels
     self.emailTextField.titleLabel.font = UIFont.systemFont(ofSize: 18)
     self.emailTextField.font = UIFont.systemFont(ofSize: 18)
@@ -44,43 +42,23 @@ class ViewController: UIViewController {
     self.passwordTextField.font = UIFont.systemFont(ofSize: 18)
     self.passwordTextField.errorLabel.font = UIFont.systemFont(ofSize: 18)
     
+    self.dataValidator = TKDataValidator(textFields: [self.emailTextField, self.passwordTextField], validators: [TKDataValidator.email, TKDataValidator.password])
+    self.dataValidator.delegate = self
+    
     // Disable submitButton at start
     self.submitButton.isEnabled = false
-  }
-  
-  func addTargetForErrorUpdating(_ textField: TKFormTextField) {
-    textField.addTarget(self, action: #selector(clearErrorIfNeeded), for: .editingChanged)
-    textField.addTarget(self, action: #selector(updateError), for: .editingDidEnd)
-  }
-  
-  func updateError(textField: TKFormTextField) {
-    textField.error = validationError(textField)
-    self.submitButton.isEnabled = isAllTextFieldsValid()
-  }
-  
-  func clearErrorIfNeeded(textField: TKFormTextField) {
-    if validationError(textField) == nil {
-      textField.error = nil
-    }
-    self.submitButton.isEnabled = isAllTextFieldsValid()
-  }
-  
-  fileprivate func isAllTextFieldsValid() -> Bool {
-    return validationError(emailTextField) == nil && validationError(passwordTextField) == nil
-  }
-  
-  private func validationError(_ textField: TKFormTextField) -> String? {
-    if textField == emailTextField {
-      return TKDataValidator.email(text: textField.text)
-    } else {
-      return TKDataValidator.password(text: textField.text)
-    }
   }
   
   @IBAction func submit(_ sender: Any) {
     self.performSegue(withIdentifier: submitSegueId, sender: nil)
   }
   
+}
+
+extension ViewController: TKDataValidatorDelegate {
+  func dataValidator(validator: TKDataValidator, isAllValid: Bool) {
+    self.submitButton.isEnabled = isAllValid
+  }
 }
 
 extension ViewController: UITextFieldDelegate {
@@ -90,7 +68,7 @@ extension ViewController: UITextFieldDelegate {
     }
     else if (textField == passwordTextField) {
       _ = passwordTextField.resignFirstResponder()
-      if isAllTextFieldsValid() {
+      if self.dataValidator.isAllValid {
         submit(submitButton)
       }
     }
