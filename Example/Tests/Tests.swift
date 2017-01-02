2180f6b97215070d630346b21c911a1a437201de
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2017-01-02 21:07:38 +0800

    Add some simple unit tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,27 +3,41 @@ import XCTest
 import TKFormTextField
 
 class Tests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    XCTAssert(true, "Pass")
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let tf = TKFormTextField(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+    XCTAssert(tf.errorLabel.text == nil)
+    XCTAssert(tf.titleLabel.text == nil)
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    tf.error = "Error"
+    XCTAssert(tf.errorLabel.text == "Error")
+    XCTAssert(tf.hasError)
+    tf.error = nil
+    XCTAssert(tf.errorLabel.text == nil)
+    XCTAssert(tf.hasError == false)
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
+    tf.title = "Custom Title"
+    XCTAssert(tf.titleLabel.text == "Custom Title")
+    tf.title = nil
+    XCTAssert(tf.titleLabel.text == nil)
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    tf.placeholder = "Custom Placeholder"
+    XCTAssert(tf.titleLabel.text == "Custom Placeholder")
+    tf.placeholder = nil
+    XCTAssert(tf.titleLabel.text == nil)
+  }
+  
 }
