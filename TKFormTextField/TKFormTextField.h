4fafb58ca10adf4371f90e80432f3f4d8b541e35
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2017-05-08 15:07:18 +0800

    Create Framework target for Carthage

---
+++ TKFormTextField/TKFormTextField.h
@@ -0,0 +1,19 @@
+//
+//  TKFormTextField.h
+//  TKFormTextField
+//
+//  Created by Thongchai Kolyutsakul on 8/5/17.
+//  Copyright © 2017 Viki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TKFormTextField.
+FOUNDATION_EXPORT double TKFormTextFieldVersionNumber;
+
+//! Project version string for TKFormTextField.
+FOUNDATION_EXPORT const unsigned char TKFormTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKFormTextField/PublicHeader.h>
+
+
